ff236e557827c7224ceb46b2d4bfe3c5ba9d26fd
danel-torre-ubuntu <dlafuente003@gmail.com>
2023-03-23 19:55:38 +0100

    zsh/bash order changed for osx

--- install.sh
+++ install.sh
@@ -1,7 +1,25 @@
 
 docker build -t pyta .
+# if it is using zsh
+if [ -n "$ZSH_VERSION" ]; then
+    # remove all aliases starting with pyta from .zshrc if it exists
+    if [ -f "$HOME/.zshrc" ]; then
+        sed -i '/^alias pyta/d' "$HOME/.zshrc"
+    fi
+    # create run.sh file and add docker run --rm -v <current_dir>:/app -it pyta
+    echo "docker run --rm -v $(pwd):/app -it pyta" > run.sh
+    # add prune.sh execution to run.sh
+    echo $(pwd)/prune.sh >> run.sh
+
+    chmod +x run.sh
+
+    # add alias pyta to .zshrc
+    echo "alias pyta='$(pwd)/run.sh'" >> "$HOME/.zshrc"
+
+    # restart zsh
+    exec zsh
 # if it is using bash
-if [ -n "$BASH_VERSION" ]; then
+elif [ -n "$BASH_VERSION" ]; then
     # remove all aliases starting with pyta from .bash_aliases if it exists
     if [ -f "$HOME/.bash_aliases" ]; then
         sed -i '/^alias pyta/d' "$HOME/.bash_aliases"
@@ -20,24 +38,7 @@ if [ -n "$BASH_VERSION" ]; then
     # restart bash
     exec bash
 
-# if it is using zsh
-elif [ -n "$ZSH_VERSION" ]; then
-    # remove all aliases starting with pyta from .zshrc if it exists
-    if [ -f "$HOME/.zshrc" ]; then
-        sed -i '/^alias pyta/d' "$HOME/.zshrc"
-    fi
-    # create run.sh file and add docker run --rm -v <current_dir>:/app -it pyta
-    echo "docker run --rm -v $(pwd):/app -it pyta" > run.sh
-    # add prune.sh execution to run.sh
-    echo $(pwd)/prune.sh >> run.sh
 
-    chmod +x run.sh
-
-    # add alias pyta to .zshrc
-    echo "alias pyta='$(pwd)/run.sh'" >> "$HOME/.zshrc"
-
-    # restart zsh
-    exec zsh
 else
     echo "Tu shell no está en la lista de shells soportadas. Puedes intentar agregarla modificando el archivo install.sh. Si lo consigues o quieres que lo hagamos, por favor abre un issue en el repositorio de GitHub."
     exit 1
--- uninstall.sh
+++ uninstall.sh
@@ -1,17 +1,5 @@
-# if it is using bash
-if [ -n "$BASH_VERSION" ]; then
-    # remove all aliases starting with pyta from .bash_aliases if it exists
-    if [ -f "$HOME/.bash_aliases" ]; then
-        sed -i '/^alias pyta/d' "$HOME/.bash_aliases"
-    fi
-    # remove run.sh file
-    rm run.sh
-
-    # restart bash
-    exec bash
-
 # if it is using zsh
-elif [ -n "$ZSH_VERSION" ]; then
+if [ -n "$ZSH_VERSION" ]; then
     # remove all aliases starting with pyta from .zshrc if it exists
     if [ -f "$HOME/.zshrc" ]; then
         sed -i '/^alias pyta/d' "$HOME/.zshrc"
@@ -21,6 +9,17 @@ elif [ -n "$ZSH_VERSION" ]; then
 
     # restart zsh
     exec zsh
+# if it is using bash
+elif [ -n "$BASH_VERSION" ]; then
+    # remove all aliases starting with pyta from .bash_aliases if it exists
+    if [ -f "$HOME/.bash_aliases" ]; then
+        sed -i '/^alias pyta/d' "$HOME/.bash_aliases"
+    fi
+    # remove run.sh file
+    rm run.sh
+
+    # restart bash
+    exec bash
 else
     echo "Tu shell no está en la lista de shells soportadas. Puedes intentar agregarla modificando el archivo uninstall.sh. Si lo consigues o quieres que lo hagamos, por favor abre un issue en el repositorio de GitHub."
     exit 1
